12a7f60a1d8d1ab574175275894a4393468327c4
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-05 21:33:48 +0300

    pure reactjs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { React, Component } from 'react';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+  render() {
+    return (
+      < h1>The author Mu'men Al-madaineh</h1>
+    );
+  }
+}
+
+export default Footer;
