a389cb60c45b1b30dc81f9b169afa55c895e498d
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-07 00:21:18 +0300

    finsh all things

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
 class HornedBeast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vote: 0
+    };
+  }
+  getVote = () => {
+    this.setState({
+      vote: this.state.vote + 1
+    });
+  }
   render() {
     return (
-      <>
-        <h2>title: {this.props.title}</h2>
-        <img src={this.props.imgUrl} alt='animals photos' ></img>
-        <p>description: {this.props.description}</p>
-        <button>Vote</button>
-        <p>vote: {this.props.vote}</p>
-      </>
+      <div>
+        <h2>title: {this.props.bestData.title}</h2>
+        <img src={this.props.bestData.image_url} alt='animals photos' ></img>
+        <p>description: {this.props.bestData.description}</p>
+        <button onClick={this.getVote}>Vote</button>
+        <p>vote: {this.state.vote}</p>
+      </div>
     );
   }
 }
