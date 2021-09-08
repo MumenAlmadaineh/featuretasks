753fa58facdce79559b402ec5c3f92a1c42f135a
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-08 23:56:31 +0300

    class 4

--- src/component/SelectedBeast.js
+++ src/component/SelectedBeast.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 class SelectedBeast extends Component {
     render() {
+        // After I understand Es6 and react 100% I will do this labs
         return (
             <div>
-                
+            
             </div>
         )
     }
